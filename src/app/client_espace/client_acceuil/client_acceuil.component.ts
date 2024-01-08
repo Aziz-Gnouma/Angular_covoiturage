57aa44f3bc9e4ecb72e3da85e46d228eae8a510e
Naim Atia <atianaim34@gmail.com>
2024-01-08 10:59:59 +0100

    ajouter maps and route

--- src/app/client_espace/client_acceuil/client_acceuil.component.ts
+++ src/app/client_espace/client_acceuil/client_acceuil.component.ts
@@ -44,6 +44,8 @@
        this.cityService.getCities().subscribe(
          (cities) => {
            this.cities = cities;
+           console.log('cities', cities);
+
          },
          (error) => {
            console.log('Failed to fetch cities', error);
@@ -71,4 +73,19 @@
 
       });
     }
+
+    public dataFields : Object = {value : 'Game'};
+    public localData : Object[] = [
+      { Id: 'Game1', Game: 'American Football' },
+      { Id: 'Game2', Game: 'Badminton' },
+      { Id: 'Game3', Game: 'Basketball' },
+      { Id: 'Game4', Game: 'Cricket' },
+      { Id: 'Game5', Game: 'Baseball' },
+      { Id: 'Game6', Game: 'Golf' },
+      { Id: 'Game7', Game: 'Hockey' },
+      { Id: 'Game8', Game: 'Rugby' },
+      { Id: 'Game9', Game: 'Snooker' },
+      { Id: 'Game10', Game: 'Tennis' }
+    ];
+
    }
